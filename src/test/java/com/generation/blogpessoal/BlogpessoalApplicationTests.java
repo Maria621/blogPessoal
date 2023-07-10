d27dddc4573ac4e4f21eaf630f5e15b0efae9f75
Maria De Fátima <133269366+Maria621@users.noreply.github.com>
2023-07-10 14:24:33 -0300

    Atualizando

--- src/test/java/com/generation/blogpessoal/BlogpessoalApplicationTests.java
+++ src/test/java/com/generation/blogpessoal/BlogpessoalApplicationTests.java
@@ -1,13 +1,8 @@
 package com.generation.blogpessoal;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogpessoalApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
